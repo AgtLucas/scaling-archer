19baf3572d25cde936789c42d4adce3fa8d7bec2
Lucas <lucas@agtlucas.com>
2015-04-30 23:47:27 -0300

    Update Item component

---
+++ src/app/components/Item.js
@@ -0,0 +1,29 @@
+import React from 'react/addons';
+
+/*
+ * @class Item
+ * @extends React.Component
+ */
+class Item extends React.Component {
+  /*
+   * @method shouldComponentUpdate
+   * @returns {Boolean}
+   */
+  shouldComponentUpdate () {
+    return React.addons.PureRenderMixin.shouldComponentUpdate.apply(this, arguments);
+  }
+
+  /*
+   * @method render
+   * @returns {JSX}
+   */
+   render () {
+    return <li className="cart-item">{this.props.item.title} - ${this.props.item.price}</li>;
+   }
+}
+
+Item.propTypes = {
+  item: React.PropTypes.object.isRequire
+};
+
+export default Item;
