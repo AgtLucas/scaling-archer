d48c48f7aaae036e0df25c06efdfa337efa7b3bb
Lucas <lucas@agtlucas.com>
2015-05-01 13:40:27 -0300

    Update Item.spec

---
+++ src/app/components/__tests__/Item.spec.js
@@ -0,0 +1,22 @@
+jest.dontMock('../Item');
+
+import React from 'react/addons';
+import Item from '../Item';
+
+var TestUtils = React.addons.TestUtils;
+var itemProp = {
+  title: 'Item 1',
+  price: 12
+};
+
+describe('Item', () => {
+  it('Renders properly', () => {
+    var item = TestUtils.renderIntoDocument(
+      <Item item={itemProp}/>
+    );
+
+    var li = TestUtils.findRenderedDOMComponentWithTag(item, 'li');
+
+    expect(li.getDOMNode().textContent).toEqual('Item 1 - $12');
+  });
+});
